8295e3e9dbb47471679cea14cb964708c47d317e
Michaël Hompus <michael@hompus.nl>
2025-07-03 18:27:09 +0200

    Add UML interaction fragment classes and extensions

---
+++ src/DendroDocs.Client/Uml/Fragments/AltSection.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+
+namespace DendroDocs.Uml.Fragments;
+
+/// <summary>
+/// Represents a section in a larger group.
+/// </summary>
+[DebuggerDisplay("AltSection {GroupType} {Label}")]
+public class AltSection : InteractionFragment
+{
+    /// <summary>
+    /// The type of group, like <c>alt</c>, <c>else</c>, etc..
+    /// </summary>
+    public string? GroupType { get; set; }
+
+    /// <summary>
+    /// The label of this section.
+    /// </summary>
+    public string? Label { get; set; }
+}
---
+++ src/DendroDocs.Client/Uml/Fragments/Interactions.cs
@@ -0,0 +1,8 @@
+namespace DendroDocs.Uml.Fragments;
+
+/// <summary>
+/// Represents a list of fragments on the same level.
+/// </summary>
+public class Interactions : InteractionFragment
+{
+}
